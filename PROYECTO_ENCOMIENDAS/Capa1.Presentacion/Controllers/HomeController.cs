6ff31038c3230118c19075d6b8806d1e0f4345e9
diegov1220 <diegovilca2000@hotmail.com>
2020-10-01 20:17:52 -0500

    Plantilla de inicio de sesion

--- PROYECTO_ENCOMIENDAS/Capa1.Presentacion/Controllers/HomeController.cs
+++ PROYECTO_ENCOMIENDAS/Capa1.Presentacion/Controllers/HomeController.cs
@@ -6,13 +6,23 @@ using System.Web.Mvc;
 
 namespace Capa1.Presentacion.Controllers
 {
+    [RoutePrefix("")]
+
     public class HomeController : Controller
     {
+        [Route("")]
         public ActionResult Index()
         {
             return View();
         }
 
+        [Route("Home")]
+        public ActionResult Home()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ PROYECTO_ENCOMIENDAS/Capa1.Presentacion/Controllers/Mantenedores/ClienteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capa1.Presentacion.Controllers.Mantenedores
+{
+    [RoutePrefix("Cliente")]
+
+    public class ClienteController : Controller
+    {
+        [Route("Registro")]
+        public ActionResult Doctor()
+        {
+            ViewBag.id = 0;
+            return View("~/Views/Mantenedores/Cliente/ClienteMantenedor.cshtml");
+        }
+    }
+}
