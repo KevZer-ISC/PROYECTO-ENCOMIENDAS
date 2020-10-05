9b625a9ff1e2270f04f8d697c890fb78a0687f58
diegov1220 <diegovilca2000@hotmail.com>
2020-10-04 20:30:14 -0500

    Home

---
+++ PROYECTO_ENCOMIENDAS/Capa1.Presentacion/Controllers/Inicio/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capa1.Presentacion.Controllers.Inicio
+{
+    [RoutePrefix("Inicio")]
+
+    public class DashboardController : Controller
+    {
+        [Route("Dashboard")]
+        public ActionResult Dashboard()
+        {
+            ViewBag.id = 0;
+            return View("~/Views/Inicio/Dashboard.cshtml");
+        }
+    }
+}
